4cce93f8670dbf45a8f7511b3bb0f367ee90151b
Moeez Ahmad <moeez0915@gmail.com>
2026-06-24 08:28:38 -0400

    Add delete functionality for activities with remove button on trip details page

--- client/src/components/ActivityBtn.jsx
+++ client/src/components/ActivityBtn.jsx
@@ -13,15 +13,25 @@ const ActivityBtn = (props) =>  {
       },
       body: JSON.stringify({num_votes: num_votes + 1})
     }
-  
-    fetch('/api/activities/' + props.id, options)
+
+    fetch(`${props.api_url}/api/activities/${props.id}`, options)
     setNumVotes((num_votes) => num_votes + 1)
   }
 
+  const deleteActivity = async (e) => {
+    e.stopPropagation()
+
+    await fetch(`${props.api_url}/api/activities/${props.id}`, { method: 'DELETE' })
+    props.onDelete(props.id)
+  }
+
   return (
-    <button className='activityBtn' id={props.id} onClick={updateCount}>
-      {props.activity} <br/> {'△ ' + num_votes + ' Upvotes' }
-    </button>
+    <div className='activityWrapper'>
+      <div className='activityBtn' id={props.id} onClick={updateCount}>
+        {props.activity} <br/> {'△ ' + num_votes + ' Upvotes' }
+        <span className='deleteActivityBtn' onClick={deleteActivity}>✕</span>
+      </div>
+    </div>
   )
 
 }
