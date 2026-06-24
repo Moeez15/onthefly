1aeda21fa062d9a54b47f872109d022b01a0a530
Moeez Ahmad <moeez0915@gmail.com>
2026-06-24 16:52:41 -0400

    update

--- server/server.js
+++ server/server.js
@@ -13,10 +13,16 @@ import aiRoutes from './routes/ai.js'
 
 const app = express()
 
+app.set('trust proxy', 1)
+
 app.use(session({
     secret: 'codepath',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        sameSite: 'none'
+    }
 }))
 app.use(express.json())
 app.use(cors({
